222d4e9d74faef6a6cfcb22d39e5d940539f623b
Atena Hatta <atn041102@gmail.com>
2022-11-18 12:24:09 -0800

    up again

--- src/dbUtil/Config.java
+++
@@ -1,9 +0,0 @@
-package dbUtil;
-
-public class Config {
-
-    protected static final String URL = "jdbc:postgresql://db.sukwmteyqstalrwzycxu.supabase.co:5432/postgres";
-    protected static final String USERNAME = "postgres";
-    protected static final String PASSWORD = "Nr3DO96agfqeHfLb";
-    
-}
